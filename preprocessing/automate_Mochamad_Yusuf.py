f7741e37f3165c395b1b8a056b759a2b4539b8b3
Mochamad Yusuf <mochyusuf100@gmail.com>
2026-05-16 16:16:45 +0700

    Change Dataset

--- preprocessing/automate_Mochamad_Yusuf.py
+++ preprocessing/automate_Mochamad_Yusuf.py
@@ -2,24 +2,26 @@ import pandas as pd
 import numpy as np
 
 if __name__ == "__main__":
-    input_file = "../covid_19_indonesia_time_series_all_raw.csv"
-    output_file = "covid_19_indonesia_time_series_all_clean.csv"
+    input_file = "../DATA RUMAH.xlsx"
+    output_file = "data_rumah_preprocessing.csv"
     
     # Read Data
-    raw_data = pd.read_csv(input_file)
+    raw_data = pd.read_excel(input_file, sheet_name='Sheet1')
     
     # Preprocessing
     df = raw_data.drop(columns=[
-        'Date','Location ISO Code', 'Location Level','City or Regency', 'Province', 'Country', 'Continent', 'Island', 'Time Zone', 'Special Status',
-        'Area (km2)', 'Population', 'Population Density', 'New Cases per Million', 'Total Cases per Million', 'New Deaths per Million',
-        'Total Deaths per Million', 'Total Deaths per 100rb', 'Growth Factor of New Cases', 'Growth Factor of New Deaths', 'Case Fatality Rate',
-        'Case Recovered Rate', 'Total Regencies', 'Total Cities', 'Total Districts', 'Total Urban Villages', 'Total Rural Villages', 'Longitude', 'Latitude',
-        'New Active Cases', 'New Cases', 'New Deaths', 'New Recovered', 'Total Active Cases'
+        'NO','NAMA RUMAH'
     ])
 
-    df = df.drop(df[df['Location'] == 'Indonesia'].index)
+    df.rename(columns={'LB': 'Luas Bangunan', 
+                   'LT': 'Luas Tanah', 
+                   'KT': 'Kamar Tidur', 
+                   'KM': 'Kamar Mandi', 
+                   'GRS': 'Garasi', }, inplace=True)
 
-    df.drop_duplicates(inplace=True)
+    df = df.dropna()
+    
+    df = df.drop_duplicates()
 
     # Save Data
-    raw_data.to_csv(output_file, index=False)
+    df.to_csv(output_file, index=False)
